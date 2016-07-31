e466c02a9f3bb3f7be94cbe778619fc31cef0dfc
Zach <zach3lee@gmail.com>
2016-07-31 13:27:49 -0700

    first commit

---
+++ board.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+# board.py
+
+class Board():
+	'Class for handling the board state. This class is ignorant of game logic rules.'
+
+	def __init__(self):
+		'Creates a blank 9x9 board and sets the current display_method.'
+		pass
+
+	def get_board(self):
+		'Returns the current board.'
+		pass
+
+	def get_cell(self, row: int, column: int):
+		'Returns the value of a cell.'
+		pass
+
+	def get_row(self, row: int):
+		'Returns the values of a row.'
+		pass
+
+	def get_column(self, column: int):
+		'Returns the values of a column.'
+		pass
+
+	def get_box(self, row: int, column: int):
+		'Returns the values of the 3x3 box that the given cell is a member of.'
+		pass
+
+	def add(self, row: int, column: int, number):
+		'Changes the value of a cell to a certain number.'
+		pass
+
+	def clear(self, row: int, column: int):
+		'Clears the given cell and returns the number that was there.'
+		pass
+
+	def to_csv(self, filename):
+		'Saves board to a csv file format at the given filename.'
+		pass
+
+	def read_csv(self, filename):
+		'Loads board from the csv file at the given filename.'
+		pass
+
+	def print_board(self):
+		'Prints the board.'
+		pass
+
+	def display_board(self, display_method=self.display_method):
+		'Displays the board, defaulting to the current display method.'
+		pass
+
+	def set_display_method(self, display_method):
+		'Sets the current display method.'
+		pass
+
+	def _box_indices(self, row: int, column: int):
+		'Returns the indices for all the members of the given 3x3 box.'
+		pass
---
+++ sudokugame.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+# sudokugame.py
+
+class Game():
+	'''Handles all the game logic and interfaces with the board execute moves'''
+
+	def __init__(self) -> :
+		'Starts a new game'
+		pass
+
+	def valid_move(self, row: int, column: int, number: int) -> bool:
+		'''
+		Returns a boolean describing whether or not the given move is valid on the board.
+		Needs to check:
+			(1) Cell is within the board's bounds
+			(2) Cell is not occupied
+			(3) The move doesn't have any of the same entry in the same column, row, or box.
+		'''
+		pass
+
+	def new_game(self):
+		'Creates a new board to start a new game'
+		pass
+
+	def get_board(self):
+		'Returns a representation of the game board.'
+		pass
+
+	def open_cells(self):
+		'Returns a list of the cells that are empty.'
+		pass
+
+	def make_move(self, row: int, column: int, number: int):
+		"Executes a move if it's valid. Otherwise, throws an error."
+		pass
+
+	def remove(self, row: int, column: int):
+		"Remove a number from the given cell"
+		pass
+
+	def save_state(self, state_name='save': str):
+		'''
+		Saves the current state. Uses state_name to name the csv file in
+		the board's to_csv call. 
+		'''
+		pass
+
+	def load_state(self, state_name='save': str):
+		'Loads a board state'
+		pass
+
+	def check_victory(self) -> bool:
+		'Checks the victory conditions have been met.'
+		pass
+
+	def print_board(self):
+		"Prints the board using the board's print_board method"
+		pass
+
+
+class OccupiedCellException(Exception):
+	'Exception for trying to make a move on an occupied cell.'
+	pass
+
+class CellOutOfBoundsException(Exception):
+	'Exception for giving coordinates that are outside of the board.'
+	pass
+
+class SameRowException(Exception):
+	'Exception for moves whose numbers have already been seen in the same row.'
+	pass
+
+class SameColumnException(Exception):
+	'Exception for moves whose numbers have already been seen in the same column.'
+	pass
+
+class SameBoxException(Exception):
+	'Exception for moves whose numbers have already been seen in the same box.'
+	pass
