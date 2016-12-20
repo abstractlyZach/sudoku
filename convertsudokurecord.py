ac63243d2cb71dfbc4ae36258519c13880c24078
Zach <zach3lee@gmail.com>
2016-12-19 23:19:33 -0800

    added documentation, added error handling, and added section for tracking names of the packs converted

--- convertsudokurecord.py
+++ convertsudokurecord.py
@@ -1,6 +1,8 @@
 # convertsudokurecord.py
 
-# Converts a .txt file into a game state .csv file that the sudoku can recognize
+# Converts a .txt file into one or more game state .csv files that the system can recognize
+
+# "python3 convertsudokurecord.py {text file input} [{destination folder}]"
 
 import sys
 import os
@@ -23,9 +25,10 @@ if __name__ == '__main__':
 		destination = sys.argv[2]
 	elif len(sys.argv) == 2:
 		destination = 'sudoku_states'
-	file_name = sys.argv[1]
-	file_prefix = file_name.split('.')[0]
-	with open(file_name, 'r', encoding='utf-8') as read_file:
+	file_path = sys.argv[1]
+	file_name = os.path.basename(file_path)
+	file_prefix = file_name.split('.')[0] # the identifier for the pack
+	with open(file_path, 'r', encoding='utf-8') as read_file:
 		for index, line in enumerate(read_file):
 			assert len(line) == 82 # 81 entries + \n
 			cell_counter = CellCounter()
@@ -36,3 +39,20 @@ if __name__ == '__main__':
 				cell_counter.increment()
 			output_file_name = file_prefix + '_{:05d}.csv'.format(index)
 			sudoku_board.to_csv(os.path.join(destination, output_file_name))
+
+	# add the current pack to the packlist and then reduce duplicates
+	current_packs = []
+	try:
+		with open('sudokupacklist.txt', 'r', encoding='utf-8') as packlist_file:
+			current_packs = packlist_file.readlines()
+			print(current_packs)
+	except FileNotFoundError:
+		pass
+	current_packs.append(file_prefix)
+	current_packs = set(current_packs)
+
+	# write the new packlist
+	with open('sudokupacklist.txt', 'w', encoding='utf-8') as packlist_file:
+		for pack in current_packs:
+			packlist_file.write(pack + '\n')
+
