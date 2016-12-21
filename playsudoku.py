adb0f63c23287d1417da1492adbf942d6bcb0550
Zach <zach3lee@gmail.com>
2016-12-21 00:18:40 -0800

    added permanency step for loading from a pre-packaged game

--- playsudoku.py
+++ playsudoku.py
@@ -14,5 +14,5 @@ sudoku_pack = commandlinegame.get_sudoku_pack_from_input()
 sudoku_number = commandlinegame.get_sudoku_number_from_input()
 game.load_state(os.path.join(constants.STATE_STORAGE_DIRECTORY, 
 							'{}_{:05d}'.format(sudoku_pack, sudoku_number)))
-
+game.set_permanent()
 commandlinegame.play(game)
