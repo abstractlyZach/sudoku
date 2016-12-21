fdf1d93cfac27152cfae0ffa7a30854b3787cd7e
Zach <zach3lee@gmail.com>
2016-12-21 01:21:33 -0800

    added loop for not finding the proper sudoku file

--- playsudoku.py
+++ playsudoku.py
@@ -9,10 +9,17 @@ import constants
 
 game = sudokugame.Game()
 turn_counter = 0
+file_found = False
 
-sudoku_pack = commandlinegame.get_sudoku_pack_from_input()
-sudoku_number = commandlinegame.get_sudoku_number_from_input()
-game.load_state(os.path.join(constants.STATE_STORAGE_DIRECTORY, 
-							'{}_{:05d}'.format(sudoku_pack, sudoku_number)))
+while not file_found:
+	try:
+		sudoku_pack = commandlinegame.get_sudoku_pack_from_input()
+		sudoku_number = commandlinegame.get_sudoku_number_from_input()
+		game.load_state(os.path.join(constants.STATE_STORAGE_DIRECTORY, 
+									'{}_{:05d}'.format(sudoku_pack, sudoku_number)))
+		file_found = True
+	except FileNotFoundError as e:
+		print(e)
+
 game.set_permanent()
 commandlinegame.play(game)
